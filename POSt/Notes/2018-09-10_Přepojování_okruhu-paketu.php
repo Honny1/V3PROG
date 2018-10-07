da52085b9d27836cacc546caa770277fc914dc43
Hony Rodák <hony.com@seznam.cz>
2018-10-07 19:48:32 +0200

    ADD Notes

---
+++ POSt/Notes/2018-09-10_Přepojování_okruhu-paketu.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+   Zapis z POSt
+        </title>
+        <meta charset="utf-8">
+        <meta content="Zapis z POSt" property="og:title">
+        <meta content="website" property="og:type">
+        <meta content="./" property="og:url">
+        <meta content="https://buchticka.eu/V3PROG/POSt/background.jpg" property="og:image">
+        <meta content="Zapis z hodiny POSt" property="og:description">
+        <link href="//cdn.muicss.com/mui-0.9.30/css/mui.min.css" rel="stylesheet" type="text/css">
+        <script src="//cdn.muicss.com/mui-0.9.30/js/mui.min.js">
+        </script>
+    </head>
+    <body>
+        <div class="mui-container">
+            <p>
+                Přepojov&aacute;n&iacute; okruhu
+            </p>
+            <ul>
+                <li>
+                    Funguje spojovaně
+                </li>
+                <li>
+                    Přenos je proudov&yacute;/blokov&yacute;
+                </li>
+                <li>
+                    Rychlost přenosu je zaručena
+                </li>
+                <li>
+                    Data se po cestě nijak nenač&iacute;taj&iacute;, neanalyzuj&iacute; neukl&aacute;daj&iacute; se nikde
+                </li>
+                <li>
+                    Ve světě telekomunikac&iacute;
+                </li>
+            </ul>
+            <p>
+                Přepojov&aacute;n&iacute; paketu
+            </p>
+            <ul>
+                <li>
+                    K&nbsp;jednotliv&yacute;m přenosům se použ&iacute;v&aacute; cel&aacute; kapacita s&iacute;tě
+                </li>
+                <li>
+                    Spojovaně i nespojovaně
+                </li>
+                <li>
+                    Rychlost přenosu nen&iacute; zaručena
+                </li>
+                <li>
+                    Snaha o efektivn&iacute; využit&iacute; s&iacute;tě
+                </li>
+                <li>
+                    Data se po cestě analyzuj&iacute; ukl&aacute;daj&iacute;, nač&iacute;taj&iacute;
+                </li>
+                <li>
+                    Maj&iacute; hlavičku
+                </li>
+                <li>
+                    Datov&eacute; s&iacute;tě
+                </li>
+            </ul>
+            <div class="paticka" style="text-align: center;">
+                <hr>
+                <p style="text-align: center; font-size: 75%; border:0%; padding:0%">
+                    Copyright &copy; 2018,
+                    <a href="https://buchticka.eu">
+                        Buchticka.eu
+                    </a>
+                    Team by Hony
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
