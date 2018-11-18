35f0cd9e5637e1978fdc165ce9a2f23f55f22d8d
Hony Rodák <hony.com@seznam.cz>
2018-11-18 23:17:45 +0100

    update notes

---
+++ POSt/Notes/2018-11-12_Adresy.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+   Zapis z POSt
+        </title>
+        <meta charset="utf-8">
+        <meta content="Zapis z POSt" property="og:title">
+        <meta content="website" property="og:type">
+        <meta content="./" property="og:url">
+        <meta content="https://buchticka.eu/V3PROG/POSt/background.jpg" property="og:image">
+        <meta content="Zapis z hodiny POSt" property="og:description">
+        <link href="//cdn.muicss.com/mui-0.9.30/css/mui.min.css" rel="stylesheet" type="text/css">
+        <script src="//cdn.muicss.com/mui-0.9.30/js/mui.min.js">
+        </script>
+    </head>
+    <body>
+        <div class="mui-container">
+            <h2>
+                <a id="_Toc501390106">
+                </a>
+                <a id="__RefHeading___Toc2742_2339991646">
+                </a>
+                Adresy
+            </h2>
+            <h3>
+                <a id="_Mac_adresa">
+                </a>
+                <a id="_Toc501390107">
+                </a>
+                <a id="__RefHeading___Toc2744_2339991646">
+                </a>
+                Mac adresa
+            </h3>
+            <ul>
+                <li>
+                    fyzick&aacute; adresa
+                </li>
+                <li>
+                    adresa linkov&eacute; vrstvy ISO/OSI
+                </li>
+                <li>
+                    jedinečn&yacute; identifik&aacute;tor s&iacute;ťov&eacute;ho zař&iacute;zen&iacute;
+                </li>
+                <li>
+                    je přidělena při v&yacute;robě
+                </li>
+                <li>
+                    je celosvětově jedinečn&aacute;
+                </li>
+                <li>
+                    48 bitů = 6 bytů
+                </li>
+            </ul>
+            <h3>
+                <a id="_Toc501390108">
+                </a>
+                <a id="__RefHeading___Toc2746_2339991646">
+                </a>
+                Segment s&iacute;tě
+            </h3>
+            <ul>
+                <li>
+                    č&aacute;st s&iacute;tě
+                </li>
+                <li>
+                    dostupn&aacute; pomoc&iacute; MAC adres
+                </li>
+            </ul>
+            <h3>
+                <a id="_Toc501390109">
+                </a>
+                <a id="__RefHeading___Toc2748_2339991646">
+                </a>
+                IPv4
+            </h3>
+            <ul>
+                <li>
+                    jednoznačně identifikuje s&iacute;ťov&eacute; rozhran&iacute; v&nbsp;PC s&iacute;ti
+                </li>
+                <li>
+                    čtyři 8 bitov&aacute; č&iacute;sla oddělen&aacute; tečkou &ndash; 32 bitů
+                </li>
+                <li>
+                    v&nbsp;des&iacute;tkov&eacute; / bin&aacute;rn&iacute; soustavě
+                </li>
+                <li>
+                    č&iacute;sla 0-255
+                </li>
+                <li>
+                    př.: 192.168.0.1 = 1101100.11110000.11000010.10000001
+                </li>
+                <li>
+                    nedostatečn&aacute; kapacita pro cel&yacute; svět
+                </li>
+                <li>
+                    nahrazena pomoc&iacute; IPv6
+                </li>
+                <li>
+                    v&nbsp;jedn&eacute; s&iacute;ti nesm&iacute; b&yacute;t 2 stejn&eacute; IP adresy
+                </li>
+                <li>
+                    přidělov&aacute;n&iacute; a spr&aacute;vu adres m&aacute; celosvětově na starosti ICANN, IANA
+                </li>
+                <li>
+                    přidělov&aacute;n&iacute; a spr&aacute;vu adres v&nbsp;ČR m&aacute; na starosti CZ.NIC
+                </li>
+                <li>
+                    dom&eacute;nov&eacute; jm&eacute;no = IP adresa v&nbsp;textov&eacute;m tvaru
+                </li>
+                <li>
+                    lok&aacute;ln&iacute; s&iacute;ť mus&iacute; m&iacute;t spr&aacute;vce &ndash; ten ji rozděluje na pods&iacute;tě a přiřazuje IP adresy
+                </li>
+                <li>
+                    struktura IP adresy &ndash; adresa s&iacute;tě | adresa uzlu
+                </li>
+                <li>
+                    adresa s&iacute;tě = 1. adresa s&iacute;tě &ndash; nesm&iacute; b&yacute;t přidělena ž&aacute;dn&eacute;mu PC v s&iacute;ti
+                </li>
+            </ul>
+            <div class="paticka" style="text-align: center;">
+                <hr>
+                <p style="text-align: center; font-size: 75%; border:0%; padding:0%">
+                    Copyright &copy; 2018,
+                    <a href="https://buchticka.eu">
+                        Buchticka.eu
+                    </a>
+                    Team by Hony
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
