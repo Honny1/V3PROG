85901dd207d9be7fd1628a14dd713c7613ff1ec0
Hony Rodák <hony.com@seznam.cz>
2019-02-17 19:55:53 +0100

    update notes

---
+++ TVY/Notes/12_Periferie.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+   Zapis z TVY
+        </title>
+        <meta charset="utf-8">
+        <meta content="Zapis z TVY" property="og:title">
+        <meta content="website" property="og:type">
+        <meta content="./" property="og:url">
+        <meta content="" property="og:image">
+        <meta content="Zapis z hodiny TVY" property="og:description">
+        <link href="//cdn.muicss.com/mui-0.9.30/css/mui.min.css" rel="stylesheet" type="text/css">
+        <script src="//cdn.muicss.com/mui-0.9.30/js/mui.min.js">
+        </script>
+    </head>
+    <body>
+        <div class="mui-container">
+            <p>
+                Periferie
+            </p>
+            <h1>
+                Tisk&aacute;rny
+            </h1>
+            <ul>
+                <li>
+                    V&yacute;stupn&iacute; zař&iacute;zen&iacute;
+                </li>
+                <li>
+                    Data můžeme přen&aacute;&scaron;et na pap&iacute;r nebo jin&eacute; m&eacute;dium jsou ovl&aacute;d&aacute;ny jazykem PCL nebo Post Skript
+                </li>
+                <li>
+                    Vytv&aacute;ř&iacute; pouze iluzi spojit&eacute;ho tisku
+                </li>
+            </ul>
+            <h2>
+                PCL
+            </h2>
+            <ul>
+                <li>
+                    Vektorov&yacute; jazyk, m&aacute; m&aacute;lo př&iacute;kazů, to co nezobraz&iacute; vektorově, zobraz&iacute; jako bitmapu
+                </li>
+                <li>
+                    Z&aacute;visl&yacute; na druhu tisk&aacute;rny
+                </li>
+                <li>
+                    Pomalej&scaron;&iacute; než PostSkript
+                </li>
+            </ul>
+            <h2>
+                PostSkript
+            </h2>
+            <ul>
+                <li>
+                    Složitěj&scaron;&iacute;, čistě vektorov&yacute;
+                </li>
+                <li>
+                    Nez&aacute;visl&aacute; na zař&iacute;zen&iacute; a jeho rozli&scaron;ovac&iacute; schopnosti
+                </li>
+            </ul>
+            <h2>
+                Parametry tisk&aacute;ren
+            </h2>
+            <ul>
+                <li>
+                    Typ tisku &ndash; jehličkov&aacute;, inkoustov&aacute;&hellip;
+                </li>
+                <li>
+                    Barevnost tisku &ndash; ČB/barevn&yacute;
+                </li>
+                <li>
+                    Rychlost tisku &ndash; počet str&aacute;nek/jednotka času
+                </li>
+                <li>
+                    Kvalita tisku &ndash; DPI
+                </li>
+                <li>
+                    Rozhran&iacute; tisk&aacute;rny &ndash; způsob připojen&iacute; (Centronics, USB, bezdr&aacute;tově&hellip;)
+                </li>
+                <li>
+                    Cena za vyti&scaron;těnou str&aacute;nku
+                </li>
+                <li>
+                    Softwarov&aacute; v&yacute;bava &ndash; ovladače pro různ&eacute; OS
+                </li>
+                <li>
+                    Velikost z&aacute;sobn&iacute;ku
+                </li>
+                <li>
+                    Vnitřn&iacute; paměť
+                </li>
+                <li>
+                    V&aacute;ha a rozměry, př&iacute;kon
+                </li>
+            </ul>
+            <h2>
+                Faktory kvality tisku
+            </h2>
+            <h3>
+                Tiskov&yacute; bod
+            </h3>
+            <ul>
+                <li>
+                    Tvoře z&nbsp;několika bodů podle požadovan&yacute;ch stupňů &scaron;edi
+                </li>
+                <li>
+                    Chce-li zobrazit 256 &uacute;rovn&iacute; &scaron;edi, tak mus&iacute;m m&iacute;t tiskov&yacute; bod, kter&yacute; m&aacute; 256 bodů (16x16)
+                </li>
+            </ul>
+            <h3>
+                DPI (Dots per inch)
+            </h3>
+            <ul>
+                <li>
+                    Počet tiskov&yacute;ch bodů na palec
+                </li>
+                <li>
+                    Rozli&scaron;ovac&iacute; schopnosti tisku
+                </li>
+                <li>
+                    Č&iacute;m vy&scaron;&scaron;&iacute; DPI, t&iacute;m jemněj&scaron;&iacute; detaily
+                </li>
+            </ul>
+            <h3>
+                PPI (Pixel per inch)
+            </h3>
+            <ul>
+                <li>
+                    Body obrazu na palec
+                </li>
+                <li>
+                    Tisk&aacute;rna nedok&aacute;že vytisknout pixel jedn&eacute; barvy
+                </li>
+                <li>
+                    Aby mohla vytisknout jeden pixel tak potřebuje tiskov&eacute; body
+                </li>
+            </ul>
+            <h3>
+                LPI (Lines per inch)
+            </h3>
+            <ul>
+                <li>
+                    Ř&aacute;dky na palec
+                </li>
+                <li>
+                    Počet linek tiskov&yacute;ch bodů
+                </li>
+                <li>
+                    Když je LPI n&iacute;zk&eacute;, tak je obraz v&yacute;razněj&scaron;&iacute; (v&yacute;razněj&scaron;&iacute; body)
+                </li>
+                <li>
+                    Když je LPI vysok&eacute;, tak je obraz jemněj&scaron;&iacute;
+                </li>
+            </ul>
+            <h3>
+                Vylep&scaron;en&iacute; tisku
+            </h3>
+            <ul>
+                <li>
+                    Lze ovlivnit objem kapek
+                </li>
+                <li>
+                    Různ&yacute; objem kapek &ndash; různ&aacute; sytost
+                </li>
+            </ul>
+            <h3>
+                Kolik PPI mohu d&aacute;t do jednoho palce?
+            </h3>
+            <p>
+                16 odst&iacute;nů barvy	4x4
+            </p>
+            <p>
+                200DPI
+            </p>
+            <p>
+                200:4=50 PPI
+            </p>
+            <h3>
+                AM rastrov&aacute;n&iacute;
+            </h3>
+            <ul>
+                <li>
+                    Amplitudov&aacute; modulace
+                </li>
+                <li>
+                    Konvenčn&iacute; polot&oacute;n
+                </li>
+                <li>
+                    Rozm&iacute;stěn&iacute; rastru (teček) je pravideln&eacute;
+                </li>
+            </ul>
+            <h3>
+                FM rastrov&aacute;n&iacute;
+            </h3>
+            <ul>
+                <li>
+                    Frekvenčn&iacute; modulace
+                </li>
+                <li>
+                    Dif&uacute;zn&iacute; rozklad
+                </li>
+                <li>
+                    Velikost v&scaron;ech teček je stejn&aacute;
+                </li>
+                <li>
+                    Tečky jsou od sebe různě vzd&aacute;len&eacute; (tmav&eacute; jsou v&iacute;c u sebe)
+                </li>
+            </ul>
+            <h3>
+                Tisk pomoc&iacute; sublimace
+            </h3>
+            <ul>
+                <li>
+                    Tisk se spojit&yacute;mi t&oacute;ny
+                </li>
+                <li>
+                    Jsme schopni ř&iacute;dit hustotu barvy
+                </li>
+            </ul>
+            <h1>
+                Rastrov&eacute; tisk&aacute;rny
+            </h1>
+            <h2>
+                Jehličkov&aacute;
+            </h2>
+            <ul>
+                <li>
+                    Apson (značka)
+                </li>
+                <li>
+                    Použ&iacute;vaj&iacute; k&nbsp;tisku tiskovou hlavu, kter&aacute; se pohybuje ze strany na stranu
+                </li>
+                <li>
+                    P&iacute;&scaron;e přes barvic&iacute; p&aacute;sku
+                </li>
+                <li>
+                    Hlava obsahuje jehličky, kter&eacute; se dotknou p&aacute;sky, a t&iacute;m se barva otiskne na pap&iacute;r
+                </li>
+                <li>
+                    Mechanick&yacute;m tlakem
+                </li>
+                <li>
+                    Pracuj&iacute; s&nbsp;proudem ASCII znaků a nezpracov&aacute;v&aacute; celou str&aacute;nku
+                </li>
+                <li>
+                    Um&iacute; vytv&aacute;řet kopie pomoc&iacute; kop&iacute;rovac&iacute;ho pap&iacute;ru
+                </li>
+                <li>
+                    Um&iacute; tisknout na tzv. nekonečn&yacute; pap&iacute;r
+                </li>
+                <li>
+                    Ř&iacute;zeno elektromagnetem (jehličky)
+                </li>
+                <li>
+                    Počet jehliček je různ&yacute; &ndash; č&iacute;m v&iacute;c t&iacute;m l&iacute;p, může b&yacute;t 7,9, až 24
+                </li>
+                <li>
+                    Někter&eacute; p&aacute;sky jsou jednor&aacute;zov&eacute; a někter&eacute; můžou b&yacute;t použity opakovaně
+                </li>
+                <li>
+                    Počet průchodů tiskov&eacute; hlavy na tisk
+                    <ul>
+                        <li>
+                            Jeden průchod hlavy &ndash; jeden znak vyti&scaron;těn &uacute;derem (např.) 24 jehliček
+                        </li>
+                        <li>
+                            Dva průchody hlavy &ndash; vyti&scaron;těna prvně půlka ř&aacute;dku &ndash; kvalitněj&scaron;&iacute; tisk
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <h2>
+                Tepeln&eacute;
+            </h2>
+            <h3>
+                Př&iacute;m&yacute; tisk
+            </h3>
+            <ul>
+                <li>
+                    Jednobarevn&yacute;
+                </li>
+                <li>
+                    Tiskov&aacute; hlava je tvořena mal&yacute;mi otvory &ndash;tepeln&aacute; setrvačnost, teplocitliv&yacute; pap&iacute;r
+                </li>
+                <li>
+                    Když se topn&eacute; těl&iacute;sko dotkne pap&iacute;ru tak v&nbsp;tom m&iacute;stě zčern&aacute;
+                </li>
+                <li>
+                    Spotřebn&iacute; materi&aacute;l je pouze teplocitliv&yacute; pap&iacute;r
+                </li>
+            </ul>
+            <h3>
+                Termotransferov&eacute;
+            </h3>
+            <ul>
+                <li>
+                    Použ&iacute;vaj&iacute; se pro jednobarevn&yacute; tisk (může i barvn&eacute;)
+                </li>
+                <li>
+                    &Scaron;t&iacute;tky, karty
+                </li>
+                <li>
+                    Tisk fotografi&iacute;
+                </li>
+                <li>
+                    Podob&aacute; se to př&iacute;m&eacute;mu tisku
+                </li>
+                <li>
+                    Mezi hlavou a pap&iacute;rem je speci&aacute;ln&iacute; termotransferov&aacute; f&oacute;lie (jednor&aacute;zov&aacute;)
+                </li>
+                <li>
+                    V&nbsp;m&iacute;stě kde je f&oacute;lie zahř&aacute;t&aacute; tak se barva vlivem tepla přenese na pap&iacute;r
+                </li>
+                <li>
+                    Drah&eacute;
+                </li>
+                <li>
+                    F&oacute;lie m&aacute; v&iacute;ce vrstev aby pokryla cel&yacute; CMYK
+                </li>
+            </ul>
+            <h3>
+                Sublimačn&iacute;
+            </h3>
+            <ul>
+                <li>
+                    Napařuje se inkoust
+                </li>
+                <li>
+                    Barvivo je na p&aacute;sce, tiskov&aacute; hlava se zahřeje (300&deg;C) p&aacute;sku??
+                </li>
+                <li>
+                    Inkoust je tuh&yacute; pak sublimuje na plyn
+                </li>
+                <li>
+                    Plyn se nat&aacute;hne do trysek (330&deg;C) a tlakem je vymr&scaron;těn plyn na pap&iacute;r a prudce se ochlad&iacute; (desublimuje)
+                </li>
+                <li>
+                    V&yacute;hoda DPI=PPI &ndash; vysok&aacute; kvalita tisku
+                </li>
+            </ul>
+            <h2>
+                Inkoustov&eacute; tisk&aacute;rny
+            </h2>
+            <ul>
+                <li>
+                    Inkoust je na pap&iacute;r vymr&scaron;ťov&aacute;n podobou kapek velkou rychlost&iacute;
+                </li>
+                <li>
+                    Kapky maj&iacute; od 7 do 35pl (pikolitrů)
+                </li>
+                <li>
+                    Mal&yacute; objem kapek = m&eacute;ně syt&yacute;, levněj&scaron;&iacute;, rychlej&scaron;&iacute;
+                </li>
+                <li>
+                    Velk&yacute; objem kapek = sytěj&scaron;&iacute;, draž&scaron;&iacute;, pomalej&scaron;&iacute;
+                </li>
+                <li>
+                    Objem kapek m&aacute; vliv na kvalitu
+                </li>
+                <li>
+                    Někter&eacute; tisk&aacute;rny maj&iacute; funkci ovlivnit velikost kapek
+                </li>
+                <li>
+                    Hlava je od pap&iacute;ru vzd&aacute;lena 1mm
+                </li>
+            </ul>
+            <h3>
+                Cartridge
+            </h3>
+            <ul>
+                <li>
+                    Z&aacute;sobn&iacute;k inkoustu
+                </li>
+                <li>
+                    Použ&iacute;v&aacute; syst&eacute;m CMYK nebo CMYKem (světlej&scaron;&iacute; varianta azurov&eacute; a purpurov&eacute;)
+                </li>
+            </ul>
+            <h3>
+                Term&aacute;ln&iacute; inkoustov&eacute;
+            </h3>
+            <ul>
+                <li>
+                    Tiskov&aacute; hlava s&nbsp;tepl&yacute;mi těl&iacute;sky a ty zahř&iacute;vaj&iacute; inkoust &ndash; vznikne bubliny
+                </li>
+                <li>
+                    Zahř&iacute;v&aacute; se, až bublina praskne a t&iacute;m vymr&scaron;t&iacute; kapky na pap&iacute;r
+                </li>
+            </ul>
+            <h3>
+                Piezoelektrick&eacute;
+            </h3>
+            <ul>
+                <li>
+                    tzv. piezoelektrick&eacute; krystaly = destička, kter&aacute; měn&iacute; svůj tvar
+                </li>
+                <li>
+                    trysky jsou uložen&eacute; v&nbsp;komůrk&aacute;ch piezoelektrick&eacute;ho krystalu
+                </li>
+            </ul>
+            <div class="paticka" style="text-align: center;">
+                <hr>
+                <p style="text-align: center; font-size: 75%; border:0%; padding:0%">
+                    Copyright &copy; 2018, by Hony
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
